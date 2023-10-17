407eec84170d42de41bb698f7bca612ffadc87f7
deidara6026 <deidarathedev24@gmail.com>
2023-10-17 13:19:06 +0000

    another fix

--- main.py
+++ main.py
@@ -10,8 +10,21 @@ async def main():
         for i in config.phone_list:
             c = Spyder(i, config.api_id, config.api_hash)
             await c.initialize()
+
             web.append(c)
-        await asyncio.gather(*[spyder._dump_all_messages(spyder.crawl_list.pop()) for spyder in web for i in range(config.spiders_per_phone)])
+        x = []
+        while Spyder.crawl_list:
+            for i in range(config.spiders_per_phone):
+                for spyder in web:
+                    if Spyder.crawl_list:  # Check if the list is not empty
+                        chat = Spyder.crawl_list.pop()
+                        x.append(spyder._dump_all_messages(chat))
+            if x:  # If there are tasks to gather
+                await asyncio.gather(*x)
+            else:
+                print("Operation complete")
+
+        
 
 if __name__=="__main__":
     asyncio.run(main())
